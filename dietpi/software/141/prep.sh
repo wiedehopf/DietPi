70f527b8a4078c03fca6cfa7e05565db5015905d
Dirk Hohndel <dirk@hohndel.org>
2024-05-20 23:53:00 +0000

    do the full adsb.im install in the prep phase
    All of this should survive the first boot, so no point in doing this at
    install time. In order to make this work for CI builds that aren't
    releases, we need to have a way to install a specific SHA of the adsb.im
    code.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- dietpi/software/141/prep.sh
+++ dietpi/software/141/prep.sh
@@ -44,41 +44,43 @@ then
     # and docker compose (which requires pip)
     apt install -y --no-install-recommends docker-compose-plugin
 
-    # and ironically - we actually do NOT install adsb.im here... we must do this on first install so that things are copied and configured
-    # the way we need it - but all of the time intensive heavy lifting is in the above
-else
-# clone the adsb-feeder repo into /tmp
-G_EXEC_OUTPUT=1 G_EXEC git clone -b dietpi 'https://github.com/dirkhh/adsb-feeder-image.git' /tmp/adsb-feeder
+    # next clone the feeder image and put the files in place, but don't complete running the install
+    echo "clone the adsb-feeder-image repo and checkout SHA \"$G_ADSB_SHA\""
+    G_EXEC_OUTPUT=1 G_EXEC git clone -b dietpi 'https://github.com/dirkhh/adsb-feeder-image.git' /tmp/adsb-feeder
+    cd /tmp/adsb-feeder
+    [[ $G_ADSB_SHA != "" ]] && git checkout "$G_ADSB_SHA"
 
-# remove the files that aren't needed for an app install on DietPi
-G_EXEC cd /tmp/adsb-feeder/src/modules/adsb-feeder/filesystem/root
-G_EXEC rm ./usr/lib/systemd/system/adsb-bootstrap.service
-G_EXEC rm ./usr/lib/systemd/system/adsb-update.service
-G_EXEC rm ./usr/lib/systemd/system/adsb-update.timer
+    # remove the files that aren't needed for an app install on DietPi
+    G_EXEC cd /tmp/adsb-feeder/src/modules/adsb-feeder/filesystem/root
+    G_EXEC rm ./usr/lib/systemd/system/adsb-bootstrap.service
+    G_EXEC rm ./usr/lib/systemd/system/adsb-update.service
+    G_EXEC rm ./usr/lib/systemd/system/adsb-update.timer
 
-# create the target directory for the app and populated with the code from the git checkout
-[[ -d '/opt/adsb' ]] && G_EXEC rm -R /opt/adsb
-G_EXEC mv /tmp/adsb-feeder/src/modules/adsb-feeder/filesystem/root/opt/adsb /opt/
-G_EXEC mkdir -p /opt/adsb/etc/systemd/system
-G_EXEC mv /tmp/adsb-feeder/src/modules/adsb-feeder/filesystem/root/usr/lib/systemd/system/* /opt/adsb/etc/systemd/system/
+    # create the target directory for the app and populated with the code from the git checkout
+    [[ -d '/opt/adsb' ]] && G_EXEC rm -R /opt/adsb
+    G_EXEC mv /tmp/adsb-feeder/src/modules/adsb-feeder/filesystem/root/opt/adsb /opt/
+    ls -lR /opt/adsb
+    G_EXEC mkdir -p /opt/adsb/etc/systemd/system
+    G_EXEC mv /tmp/adsb-feeder/src/modules/adsb-feeder/filesystem/root/usr/lib/systemd/system/* /opt/adsb/etc/systemd/system/
+    G_EXEC cd /tmp/adsb-feeder
+    l_ADSB_FEEDER_TAG_COMPONENT=$(git describe --match 'v[0-9]*' --long | sed 's/-[0-9]*-g[0-9a-f]*//')
 
-# set the 'image name' and version that are shown in the footer of the Web UI
-G_EXEC cd /opt/adsb
-G_EXEC eval 'echo '\''ADSB Feeder app running on DietPi'\'' > feeder-image.name'
-l_ADSB_FEEDER_TAG_COMPONENT=$(git describe --match 'v[0-9]*' --long | sed 's/-[0-9]*-g[0-9a-f]*//')
-G_EXEC eval "echo '$l_ADSB_FEEDER_TAG_COMPONENT(dietpi)' > adsb.im.version"
-unset l_ADSB_FEEDER_TAG_COMPONENT
+    # set the 'image name' and version that are shown in the footer of the Web UI
+    G_EXEC cd /opt/adsb
+    G_EXEC eval 'echo '\''ADSB Feeder app running on DietPi'\'' > feeder-image.name'
+    G_EXEC eval "echo '$l_ADSB_FEEDER_TAG_COMPONENT(dietpi)' > adsb.im.version"
+    unset l_ADSB_FEEDER_TAG_COMPONENT
 
-# remove the git clone of the repo we installed from
-G_EXEC rm -R /tmp/adsb-feeder
+    # remove the git clone of the repo we installed from
+    G_EXEC rm -R /tmp/adsb-feeder
 
-# install Python Flask
-# for older distros we need to install via pip in order to get flask 2
-# shellcheck disable=SC2154  # this file is sourced into dietpi-software
-if (( $G_DISTRO < 7 ))
-then
-    G_EXEC_OUTPUT=1 G_EXEC pip3 install -U flask
-else
-    G_AGI python3-flask
-fi
+    # install Python Flask
+    # for older distros we need to install via pip in order to get flask 2
+    # shellcheck disable=SC2154  # this file is sourced into dietpi-software
+    if (( $G_DISTRO < 7 ))
+    then
+        G_EXEC_OUTPUT=1 G_EXEC pip3 install -U flask
+    else
+        G_AGI python3-flask
+    fi
 fi
