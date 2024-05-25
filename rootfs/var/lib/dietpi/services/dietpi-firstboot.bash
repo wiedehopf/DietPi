1664407a444b52fa2a34b980e9ef8a9e209b07bd
Dirk Hohndel <dirk@hohndel.org>
2024-05-25 21:02:34 +0000

    don't make ethernet and wifi mutually exclusive
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- rootfs/var/lib/dietpi/services/dietpi-firstboot.bash
+++ rootfs/var/lib/dietpi/services/dietpi-firstboot.bash
@@ -272,8 +272,7 @@ _EOF_
 		local static_dns=$(sed -n '/^[[:blank:]]*AUTO_SETUP_NET_STATIC_DNS=/{s/^[^=]*=//p;q}' /boot/dietpi.txt)
 
 		# - WiFi
-		if (( $wifi_enabled ))
-		then
+		if false; then # this is nuts... (( $wifi_enabled )); then
 			# Enable WiFi, disable Ethernet
 			ethernet_enabled=0
 			sed --follow-symlinks -Ei "/(allow-hotplug|auto)[[:blank:]]+wlan/c\allow-hotplug $iface_wlan" /etc/network/interfaces
@@ -286,8 +285,8 @@ _EOF_
 			/boot/dietpi/func/dietpi-set_hardware wificountrycode
 
 		# - Ethernet
-		elif (( $ethernet_enabled ))
-		then
+		elif false; then  # nope, not doing that either (( $ethernet_enabled )); then
+
 			# Enable Ethernet, disable WiFi
 			wifi_enabled=0
 			sed --follow-symlinks -Ei "/(allow-hotplug|auto)[[:blank:]]+eth/c\allow-hotplug $iface_eth" /etc/network/interfaces
@@ -298,6 +297,18 @@ _EOF_
 
 			# RPi: Keep onboard WiFi enabled until end of first run installs: https://github.com/MichaIng/DietPi/issues/5391
 			(( $G_HW_MODEL > 9 )) || /boot/dietpi/func/dietpi-set_hardware wifimodules onboard_enable
+		else
+			# since we want a reasonably generic image, this MUST be able to work on either ethernet or wifi
+			# there is absolutely no excuse for preventing that
+			sed --follow-symlinks -Ei "/(allow-hotplug|auto)[[:blank:]]+wlan/c\allow-hotplug $iface_wlan" /etc/network/interfaces
+			sed --follow-symlinks -Ei "/(allow-hotplug|auto)[[:blank:]]+eth/c\allow-hotplug $iface_eth" /etc/network/interfaces
+			ethernet_enabled=1
+			wifi_enabled=1
+			# Apply global SSID/keys from dietpi.txt to wpa_supplicant
+			/boot/dietpi/func/dietpi-wifidb 1
+			# Set WiFi country code
+			/boot/dietpi/func/dietpi-set_hardware wificountrycode
+
 		fi
 
 		# - Static IP
@@ -306,9 +317,9 @@ _EOF_
 			if (( $wifi_enabled ))
 			then
 				sed --follow-symlinks -i "/iface wlan/c\iface $iface_wlan inet static" /etc/network/interfaces
+			fi
+			if (( $ethernet_enabled )); then
 
-			elif (( $ethernet_enabled ))
-			then
 				sed --follow-symlinks -i "/iface eth/c\iface $iface_eth inet static" /etc/network/interfaces
 			fi
 			sed --follow-symlinks -i "/address/c\address $static_ip" /etc/network/interfaces
@@ -330,7 +341,8 @@ _EOF_
 		# - Configure enabled interfaces now, /etc/network/interfaces will be effective from next boot on
 		#	Failsafe: Bring up Ethernet, whenever WiFi is disabled or fails to be configured, e.g. due to wrong credentials
 		# shellcheck disable=SC2015
-		(( $wifi_enabled )) && ifup "$iface_wlan" || ifup "$iface_eth"
+		(( $wifi_enabled )) && ifup "$iface_wlan"
+		(( $ethernet_enabled )) && ifup "$iface_eth"
 
 		# - Boot wait for network
 		/boot/dietpi/func/dietpi-set_software boot_wait_for_network "$(( ! $(grep -cm1 '^[[:blank:]]*AUTO_SETUP_BOOT_WAIT_FOR_NETWORK=0' /boot/dietpi.txt) ))"
